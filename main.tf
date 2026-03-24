60fdd6c22dc40624e5749d3cf44f734b05948f6e
luizfernando <lf.nandooliveeira@gmail.com>
2026-03-24 13:43:45 -0300

    criando arquivos base do tf e configurando o provider da azure

---
+++ main.tf
@@ -0,0 +1,62 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "devopsautomation-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "devopsautomation-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "devopsautomation-public-ip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "devopsautomation-nic"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "devopsautomation-vm"
+  location                        = data.azurerm_resource_group.rg.location
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  size                            = "D2ads_v7"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  disable_password_authentication = false
+
+  os_disk {
+    name                 = "devopsautomation-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "24.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
